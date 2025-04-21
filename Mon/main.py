378322002e6fa2a13a295230cf519124d60ae216
Monspiritx <historiayuki@gmail.com>
2025-04-21 22:38:32 +0700

    committ

--- Mon/main.py
+++ Mon/main.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import numpy as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+df = pd.read_csv('C:\\Users\\hp\\Mondata\\NVIDIA_Stock_Volatility_2014_2024.csv')
+
+df.info()
